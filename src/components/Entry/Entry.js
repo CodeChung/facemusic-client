68a3ba5816f2f8f16d214209590c5c73b4b89531
Harry Chung <hc9825@gmail.com>
2019-10-07 22:53:26 -0700

    entry view

--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import Donut from '../Donut/Donut';
+import styled from 'styled-components';
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHandPointLeft } from '@fortawesome/free-solid-svg-icons';
 import './Entry.css'
+
+const Image = styled.div`
+    background-image: url(${props => props.img}) ;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    width: 50vh;
+    height: 50vh;
+    border: 5px solid #1A1916;
+    border-radius: 5px 5px 0 0;
+`
 
 class Entry extends React.Component {
     state = {
@@ -18,22 +33,25 @@ class Entry extends React.Component {
     render() {
         const url = this.props.entry ? this.props.entry.song.url.replace('track', 'embed/track') : ''
         return (
-            <div>
-                {this.props.calendar && <button onClick={() => this.props.resetCalendar()}>Return to Calendar</button>}
+            <section className='entry-view'>
+                {this.props.calendar && <FontAwesomeIcon className='calendar-back-arrow' icon={faHandPointLeft} onClick={() => this.props.resetCalendar()} />}
                 <div className='entry'>
-                    {this.props.entry && <img src={this.props.entry.img} alt='daily face'/>}
-                    <div className='donut'>
-                        {/* <h3>Emotional Analysis</h3> */}
-                        <Donut emotions={this.state.emotions}/>
+                    <div className='entry-face'>
+                        <Image img={this.props.entry ? this.props.entry.img : ''} />
                         <iframe title='spotify' src={url} frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
                     </div>
-                    
+                    <div className='entry-graph'>
+                        <div className='donut'>
+                            <h3>Emotional Analysis</h3>
+                            <Donut emotions={this.state.emotions}/>
+                        </div>
+                        <div className='entry-notes'>
+                            <h3>Notes:</h3>
+                            {this.props.entry && this.props.entry.notes}
+                        </div>
+                    </div>
                 </div>
-                <div className='entry-notes'>
-                    <h3>Notes:</h3>
-                    {this.props.entry && this.props.entry.notes}
-                </div>
-            </div>
+            </section>
         )
     }
 }
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,7 +7,7 @@ function LandingInfo() {
         <section className='landing-main'>
             <div className='landing-desc'>
                 <h1>Facejams</h1>
-                <h4>It's time to listen to your face.</h4>
+                <h4>It's time to listen to that face.</h4>
                 <p>Facejams is a photojournal/playlist generator.</p>
                 <p>It analyzes the emotions featured on your face and returns music that reflects how you feel.</p>
                 <p>Use it to keep track of your moods, find music you like, and look back at memories.</p>
